5a2129a9bbb84c8a7cedb3779f4aef8f544f0ae8
XiangXu1317 <126554942+XiangXu1317@users.noreply.github.com>
2023-12-22 17:18:10 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     name: 'Xiang Technologies',
     description: 'The home store for Kasm supported workspaces.',
     icon: ' https://xiangxu1317.github.io/test_kasm_workspace_registry/1.0/gimp.png',
-    listUrl: ' https://xiangxu1317.github.io/test_kasm_workspace_registry/',
+    listUrl: ' https://xiangxu1317.github.io/test_kasm_workspace_registry_/',
     contactUrl: ' https://xiangxu1317.github.io/test_kasm_workspace_registry/issues',
   },
   reactStrictMode: true,
